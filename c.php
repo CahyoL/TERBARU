04a9232c72117211d1ceacc7721c07485051b0fb
cahyoluhur <54984275+cahyoluhur@users.noreply.github.com>
2019-09-14 22:57:14 +0700

    Add files via upload

---
+++ c.php
@@ -0,0 +1,38 @@
+<?php
+$username = "WARGA";
+$password = "+62";
+
+system("clear");
+echo "\033[31;1mlogin Dulu Gan\n";
+echo "\033[37;1musername : ";
+$user = trim(fgets(STDIN));
+echo "\033[31;1mpassword : ";
+$pass = trim(fgets(STDIN));
+if($user == username && $pass == password)
+{ 
+echo "\033[37;1mlogin sukses cuk/n";
+sleep (3);
+system("clear")
+echo "\033[31;1mTools Karangan Mr.V3c\n";
+echo "\033[37;1m[1]Tools FB
+[2]Tools-C
+[3]SPAM CALL tanpa batas/n";
+$menu = trim(fgets(STDIN));
+if($menu == 1)
+{
+system('git clone https://github.com/cahyoluhur/DarkTools');
+}
+if($menu == 2)
+system('git clone https://github.com/cahyoluhur/CL');
+{    
+if($menu == 3)
+system('git clone git clone https://github.com/Muhamadba/MBFz');
+    
+}
+else
+{
+echo "\033[32;1mlogin gagal tod/n";
+}
+
+
+?>
