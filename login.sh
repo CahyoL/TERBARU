721d5ecbe58d20ac3d53f8bbc5150cfdfe569fdd
cahyoluhur <54984275+cahyoluhur@users.noreply.github.com>
2019-09-14 23:02:01 +0700

    Update login.sh

--- login.sh
+++ login.sh
@@ -6,9 +6,9 @@ clear
 echo "\033[37;1mSELAMAT SIANG, MALAM, SORE, SUBUH.";
 sleep(1)
 echo "\033[31;1mmau PWnya";
-sleep(1);
+sleep(1)
 echo "\033[32;1mhub Contact:081365443940";
-sleep(1);
+sleep(1)
 By:Mr.Vins3ct
-sleep(1);
+sleep(1)
 echo "\033[31;1mJangan Lupa Sholat 5 Waktu Bagi Yang Beragama Islam";
